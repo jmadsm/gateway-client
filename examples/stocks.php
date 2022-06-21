5e4088dabb116ef8b487e5f10cafddf715b4956e
Andreas <aka@jma.dk>
2022-06-21 14:05:01 +0200

    add stock example

---
+++ examples/stocks.php
@@ -0,0 +1,32 @@
+<?php
+
+// Require the composer autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/config.php';
+
+use JmaDsm\GatewayClient\Client;
+use JmaDsm\GatewayClient\ApiObjects\Stock;
+
+// Configure the client singleton, this allows
+// all the ApiObjects to perform requests using
+// this configuration
+Client::getInstance(
+    $config['base_url'],
+    $config['access_token'],
+    $config['tenant_token']
+);
+
+/*
+$contacts = Contact::all(1);
+$contacts = Contact::since("2021-10-05");
+
+while($contact = $contacts->next()) {
+    var_dump($contact->name);
+}
+*/
+
+var_dump(
+//   Contact::all()->next(),
+//   Contact::since("2021-10-05")->next(),
+   Stock::get('OR2020')->next()
+);
