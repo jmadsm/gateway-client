f46cd1b1c5f1b67d3014119e16fa997ed86d27f3
Andreas <aka@jma.dk>
2022-07-05 10:38:35 +0200

    create shadow products

---
+++ src/ApiObjects/ShadowProduct.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace JmaDsm\GatewayClient\ApiObjects;
+
+use JmaDsm\GatewayClient\ApiObjectResult;
+use JmaDsm\GatewayClient\Client;
+
+class ShadowProduct
+{
+    private static string $apiPath = '/product/api/v1';
+
+    /**
+     * Returns all shadow products
+     *
+     * @param int $page
+     * @param $since
+     * @return JmaDsm\GatewayClient\ApiObjectResult;
+     */
+    public static function all(int $page = 1, $since = null)
+    {
+        $result = json_decode(Client::getInstance()->get(self::$apiPath . '/shadowproducts', ['page' => $page, 'since' => $since]));
+
+        return new ApiObjectResult($result, __METHOD__, $page, [$since]);
+    }
+
+    /**
+     * Returns specific shadow product
+     *
+     * @param $id
+     * @return ApiObjectResult
+     */
+    public static function get($id)
+    {
+        $result = json_decode(Client::getInstance()->get(self::$apiPath . '/shadowproducts/' . $id));
+
+        return new ApiObjectResult($result);
+    }
+
+    /**
+     * Returns shadow products changed since $from date. Defaults to page 1
+     *
+     * @param $since
+     * @param int $page
+     * @return ApiObjectResult
+     */
+    public static function since($since, $page = 1)
+    {
+        return Product::all($page, $since);
+    }
+}
